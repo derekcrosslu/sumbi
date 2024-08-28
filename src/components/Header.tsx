e7727b8ede3e5c904c0fb3aee0d6dad19b6fc65b
Derek Cross <derekcrosslu@gmail.com>
2024-08-28 16:21:40 -0500

    changing logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -24,15 +24,15 @@ export const Header: React.FC = () => {
     <header className='bg-white flex shadow-sm  w-full'>
       <div className='flex px-10 py-2 justify-center items-center'>
         <img
-          src='https://camo.githubusercontent.com/9e24d9896a7ec149d8eee755818f317df634e0f0ecc8d88496c63f911721a28a/68747470733a2f2f64726976652e676f6f676c652e636f6d2f75633f6578706f72743d766965772669643d314f677979534b55514a74454a4f455330486d385765512d626849336e63635949'
+          src='/SumbiLogo.png'
           alt='Sumbi'
-          className='w-20 h-23'
+          // className='w-10 h-13'
         />
       </div>
       <div className='bg-white  px-6 flex justify-between items-center w-full'>
         <div className='p-2'>
-          <h2 className='text-2xl font-semibold'>Hola! </h2>
-          <p className='text-gray-500'>Bienvenido Sumbi Dashboard</p>
+          {/* <h2 className='text-2xl font-semibold'>Hola! </h2>
+          <p className='text-gray-500'>Bienvenido Sumbi Dashboard</p> */}
         </div>
         <div className='flex items-center space-x-14 px-4'>
           <div className='gap-3 flex'>
